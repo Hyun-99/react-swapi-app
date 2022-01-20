64bc0a9d6ae1b30adc91ca62a5e12c45e101838a
Hyun <ncj223@naver.com>
2022-01-20 21:11:53 +0900

    fetch-movies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import { Fragment } from "react/cjs/react.production.min";
-import "./App.css";
+import styles from "./App.module.css";
+import Card from "./components/UI/Card";
+import Button from "./components/UI/Button";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <Fragment></Fragment>;
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovieHandler = () => {
+    fetch("https://swapi.dev/api/films")
+      .then((response) => response.json())
+      .then((result) => setMovies(result.results));
+  };
+
+  return (
+    <Fragment>
+      <Card className={styles["home-card"]}>
+        <Button onClick={fetchMovieHandler}>Fetch Movies</Button>
+      </Card>
+    </Fragment>
+  );
 }
 
 export default App;
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import styles from "./Button.module.css";
 
 const Button = (props) => {
-  return <button className={styles.btn}>{props.children}</button>;
+  return (
+    <button
+      className={`${styles.btn} ${props.className}`}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
